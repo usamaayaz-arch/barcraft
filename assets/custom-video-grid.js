0b114f67f7c565639f602fbc01d48946f0528002
usamaayaz-arch <usama.ayaz@sanomads.com>
2026-02-24 12:52:11 +0500

    video grid section changes (Js file)

--- assets/custom-video-grid.js
+++ assets/custom-video-grid.js
@@ -3,9 +3,40 @@ class CustomVideoGrid extends HTMLElement {
     super();
   }
 
-  connectedCallBack() {
-    let videoFrame = document.getElementById("videoFrameEditor");
-    videoFrame.controls = false;
+  connectedCallback() {
+
+    const videoItems = this.querySelectorAll(".video-item");
+    videoItems.forEach((item) => {
+      const video = item.querySelector("video");
+      if (!video) return;
+
+      video.playbackRate = parseFloat(item.dataset.speed) || 1;
+
+      if (item.dataset.type === "default") {
+        if (item.dataset.action === "play") {
+          video.play();
+          item.classList.add("is-playing");
+        } else if (item.dataset.action === "pause") {
+          video.pause();
+          item.classList.remove("is-playing");
+        }
+      }
+      item.addEventListener("click", (e) => {
+        e.stopPropagation();
+        console.log("CLICK", video);
+        if (video.paused) {
+          video.play();
+          item.classList.add("is-playing");
+        } else {
+          video.pause();
+          item.classList.remove("is-playing");
+        }
+      });
+      video.addEventListener("ended", () => {
+        item.classList.remove("is-playing");
+      });
+    });
   }
 }
+
 customElements.define("custom-video-grid", CustomVideoGrid);
